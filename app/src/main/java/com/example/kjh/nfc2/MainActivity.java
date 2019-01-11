aedf6f170703039bc4a82aad5848b8fd02fd0f24
KimJangHyeon <rlawkdgus75@gmail.com>
2019-01-11 18:13:19 +0900

    msg

--- app/src/main/java/com/example/kjh/nfc2/MainActivity.java
+++ app/src/main/java/com/example/kjh/nfc2/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.kjh.nfc2;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.nfc.tech.MifareUltralight;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private static String tagNum = null;
     private TextView tagDesc1;
     private TextView tagDesc2;
+
+    private Tag mTag;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,14 +64,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
 
-        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-        if (tag != null) {
-            byte[] tagId = tag.getId();
+        Tag mTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        String msg = "";
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for (int i = 0; i < rawMessages.length; i++) {
+                    messages[i] = (NdefMessage) rawMessages[i];
+                    msg = "" + messages[i].toString();
+                }
+                // Process the messages array.
+            }
+        }
+
+        if (mTag != null) {
+            byte[] tagId = mTag.getId();
             tagDesc1.setText("TagId: " + toHexString(tagId));
-            tagDesc2.setText("TagId: " + tagId);
             tagNum = toHexString(tagId);
         }
+        if (mTag != null) {
+            //String content = readTag(mTag);
+
+            tagDesc2.setText("Tag Content: " + msg);
+
+        }
+
+
+
     }
+
+    public String readTag(Tag tag) {
+
+        MifareUltralight mifare = MifareUltralight.get(tag);
+        try {
+            mifare.connect();
+            byte[] payload = mifare.readPages(4);
+            return new String(payload, Charset.forName("US-ASCII"));
+        } catch (IOException e) {
+            Log.e(TAG, "IOException while reading MifareUltralight message...", e);
+        } finally {
+            if (mifare != null) {
+                try {
+                    mifare.close();
+                }
+                catch (IOException e) {
+                    Log.e(TAG, "Error closing tag...", e);
+                }
+            }
+        }
+        return null;
+    }
+
+
     public static final String CHARS = "01233456789ABCDEF";
     public static String toHexString(byte[] data) {
         StringBuilder sb = new StringBuilder();
@@ -67,4 +129,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return sb.toString();
     }
+
+
 }
